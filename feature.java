183b79b117ed37ac5db7e247e27e085d82d6dff7
satyajeet787 <satyajeetpawar787@gmail.com>
2025-08-23 13:40:03 +0100

    new code and task added in feature.java

--- feature.java
+++ feature.java
@@ -3,12 +3,18 @@ public class Feature {
     public static void main(String[] args) {
         System.out.println("Initial version");
 
-	// Task-120: Add logout feature
+        // Task-120: Add logout feature
         logout();
+
+        // Task-101: Add login feature
+        login("user", "pass");
     }
 
     public static void logout() {
         System.out.println("Logging out...");
     }
-}
 
+    public static void login(String username, String password) {
+        System.out.println("Logging in with " + username);
+    }
+}
