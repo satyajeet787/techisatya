c6ecda49a4d78895a94d488e575bca1c3ba8b5a4
satyajeet787 <satyajeetpawar787@gmail.com>
2025-08-23 13:35:13 +0100

    Completed task-120 logout feature

--- feature.java
+++ feature.java
@@ -2,6 +2,13 @@
 public class Feature {
     public static void main(String[] args) {
         System.out.println("Initial version");
+
+	// Task-120: Add logout feature
+        logout();
+    }
+
+    public static void logout() {
+        System.out.println("Logging out...");
     }
 }
 
